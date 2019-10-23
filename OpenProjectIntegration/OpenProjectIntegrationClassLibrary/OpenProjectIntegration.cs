e3545695e4c117245f0eccb1e5b98a4f6f2192d4
Leonardo Davinc Silva <davinc131@hotmail.com>
2019-10-23 01:16:25 -0300

    Alterações para conexão com o servidor.

--- OpenProjectIntegration/OpenProjectIntegrationClassLibrary/OpenProjectIntegration.cs
+++ OpenProjectIntegration/OpenProjectIntegrationClassLibrary/OpenProjectIntegration.cs
@@ -14,10 +14,12 @@ namespace OpenProjectIntegrationClassLibrary
     public class OpenProjectIntegration
     {
         private string StringUri { get; set; }
-        private string JsonString { get; set; }
+        private string StringTeste { get; set; }
         private IRestClient _restClient;
         private IRestRequest _restRequest;
         CredentialCache credential;
+        private string[] AuthUser = { "leonardo.suporte@brgaapempresarial.com.br", "u3r5v8k9cwporw" };
+        private string[] AuthServer = { "bitnami", "leonardo" };
         private string[] Auth = { "leonardo.suporte@brgaapempresarial.com.br", "u3r5v8k9cwporw" };
         private string[] Authkey = { "leonardo.suporte@brgaapempresarial.com.br", "ae0444e17dd5faf7a4e49fcdabb4fc94d7feb50ed61dfb1ef43735049a5c0572" };
 
@@ -32,8 +34,10 @@ namespace OpenProjectIntegrationClassLibrary
         private void SetParameters()
         {
             StringUri = "http://167.99.229.202:80";
+            StringTeste = "http://192.168.0.31";
             credential = new CredentialCache();
-            credential.Add(new Uri("http://167.99.229.202/?/"), "Basic", new NetworkCredential(Auth[0], Auth[1]));
+            //credential.Add(new Uri("http://167.99.229.202/?/"), "Basic", new NetworkCredential(Auth[0], Auth[1]));
+            credential.Add(new Uri("http://192.168.0.31"), "Basic", new NetworkCredential(AuthServer[0], AuthServer[1]));
             _restRequest = new RestRequest();
             _restClient = new RestClient();
         }
